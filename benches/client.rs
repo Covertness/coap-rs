23ef87109b4f8f84129da4ec3aef83775da5e77a
Covertness <wuyingfengsui@gmail.com>
2015-07-25 19:26:31 +0800

    bench client request

---
+++ benches/client.rs
@@ -0,0 +1,26 @@
+#![feature(test)]
+
+extern crate test;
+extern crate coap;
+
+use test::Bencher;
+use coap::packet::*;
+use coap::CoAPClient;
+
+#[bench]
+fn bench_client_request(b: &mut Bencher) {
+	let addr = "127.0.0.1:5683";
+	let request = "test";
+	let mut packet = Packet::new();
+	packet.header.set_version(1);
+	packet.header.set_type(PacketType::Confirmable);
+	packet.header.set_code("0.01");
+	packet.header.set_message_id(1);
+	packet.set_token(vec!(0x51, 0x55, 0x77, 0xE8));
+	packet.add_option(OptionType::UriPath, request.to_string().into_bytes());
+
+	b.iter(|| {
+		let client = CoAPClient::new(addr).unwrap();
+		client.send(&packet).unwrap();
+	});
+}
