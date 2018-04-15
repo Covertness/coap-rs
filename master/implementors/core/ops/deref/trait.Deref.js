e54c64294bc7751f380c771deff876ac2bf09aca
Travis CI User <travis@example.org>
2018-04-15 12:00:35 +0000

    Automatic Travis documentation build

--- master/implementors/core/ops/deref/trait.Deref.js
+++ master/implementors/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bincode"] = ["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"bincode/struct.RefBox.html\" title=\"struct bincode::RefBox\">RefBox</a>&lt;'a, T&gt;",];
+implementors["bincode"] = [{text:"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"bincode/struct.RefBox.html\" title=\"struct bincode::RefBox\">RefBox</a>&lt;'a, T&gt;",synthetic:false,types:["bincode::refbox::RefBox"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
