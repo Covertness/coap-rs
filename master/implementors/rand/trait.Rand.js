ed2d6b650e7e68b68df9e28e26b95e7c0c732a81
Travis CI User <travis@example.org>
2018-04-15 11:39:16 +0000

    Automatic Travis documentation build

--- master/implementors/rand/trait.Rand.js
+++ master/implementors/rand/trait.Rand.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["rand"] = [];
-implementors["uuid"] = ["impl <a class=\"trait\" href=\"rand/trait.Rand.html\" title=\"trait rand::Rand\">Rand</a> for <a class=\"struct\" href=\"uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>",];
+implementors["uuid"] = [{text:"impl <a class=\"trait\" href=\"rand/trait.Rand.html\" title=\"trait rand::Rand\">Rand</a> for <a class=\"struct\" href=\"uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>",synthetic:false,types:["uuid::Uuid"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
