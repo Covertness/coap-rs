5d93d53e017f43e39f63aab0d6ee09c2548e6661
Travis CI User <travis@example.org>
2018-04-15 11:59:27 +0000

    Automatic Travis documentation build

--- master/storage.js
+++ master/storage.js
@@ -1,4 +1,3 @@
-var resourcesSuffix = "";
 /*!
  * Copyright 2018 The Rust Project Developers. See the COPYRIGHT
  * file at the top-level directory of this distribution and at
@@ -13,18 +12,6 @@ var resourcesSuffix = "";
 
 var currentTheme = document.getElementById("themeStyle");
 var mainTheme = document.getElementById("mainThemeStyle");
-
-var savedHref = [];
-
-function onEach(arr, func) {
-    if (arr && arr.length > 0 && func) {
-        for (var i = 0; i < arr.length; i++) {
-            if (func(arr[i]) === true) {
-                break;
-            }
-        }
-    }
-}
 
 function updateLocalStorage(name, value) {
     if (typeof(Storage) !== "undefined") {
@@ -42,30 +29,8 @@ function getCurrentValue(name) {
 }
 
 function switchTheme(styleElem, mainStyleElem, newTheme) {
-    var fullBasicCss = "rustdoc" + resourcesSuffix + ".css";
-    var fullNewTheme = newTheme + resourcesSuffix + ".css";
-    var newHref = mainStyleElem.href.replace(fullBasicCss, fullNewTheme);
-
-    if (styleElem.href === newHref) {
-        return;
-    }
-
-    var found = false;
-    if (savedHref.length === 0) {
-        onEach(document.getElementsByTagName("link"), function(el) {
-            savedHref.push(el.href);
-        });
-    }
-    onEach(savedHref, function(el) {
-        if (el === newHref) {
-            found = true;
-            return true;
-        }
-    });
-    if (found === true) {
-        styleElem.href = newHref;
-        updateLocalStorage('rustdoc-theme', newTheme);
-    }
+    styleElem.href = mainStyleElem.href.replace("rustdoc.css", newTheme + ".css");
+    updateLocalStorage('theme', newTheme);
 }
 
-switchTheme(currentTheme, mainTheme, getCurrentValue('rustdoc-theme') || 'light');
+switchTheme(currentTheme, mainTheme, getCurrentValue('theme') || 'main');
