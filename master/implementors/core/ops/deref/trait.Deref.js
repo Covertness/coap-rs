72f2bd8c953ffb367b6fd9f123a17d06a0d10a2a
Travis CI User <travis@example.org>
2018-08-11 05:38:54 +0000

    Automatic Travis documentation build

--- master/implementors/core/ops/deref/trait.Deref.js
+++ master/implementors/core/ops/deref/trait.Deref.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["bincode"] = [{text:"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"bincode/struct.RefBox.html\" title=\"struct bincode::RefBox\">RefBox</a>&lt;'a, T&gt;",synthetic:false,types:["bincode::refbox::RefBox"]},];
+implementors["regex_syntax"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"regex_syntax/hir/literal/struct.Literal.html\" title=\"struct regex_syntax::hir::literal::Literal\">Literal</a>",synthetic:false,types:["regex_syntax::hir::literal::Literal"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ master/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["regex_syntax"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"regex_syntax/hir/literal/struct.Literal.html\" title=\"struct regex_syntax::hir::literal::Literal\">Literal</a>",synthetic:false,types:["regex_syntax::hir::literal::Literal"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
