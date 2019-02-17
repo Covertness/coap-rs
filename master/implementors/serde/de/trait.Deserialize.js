4aaaa5bcd47cdcc02aa18d866e764a484f96d204
Travis CI User <travis@example.org>
2019-02-17 10:30:05 +0000

    Automatic Travis documentation build

---
+++ master/implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["coap"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"coap/message/header/struct.HeaderRaw.html\" title=\"struct coap::message::header::HeaderRaw\">HeaderRaw</a>",synthetic:false,types:["coap::message::header::HeaderRaw"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["coap"] = [{text:"impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"coap/message/header/struct.HeaderRaw.html\" title=\"struct coap::message::header::HeaderRaw\">HeaderRaw</a>",synthetic:false,types:["coap::message::header::HeaderRaw"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
