c3c0a9113ee21389fecf2bc63926f9a055be95ff
Covertness <wuyingfengsui@gmail.com>
2019-12-23 21:35:31 +0800

    fix bench

--- benches/server.rs
+++ benches/server.rs
@@ -2,17 +2,24 @@
 
 extern crate test;
 
-use std::thread;
+use std::{
+    thread,
+    sync::mpsc,
+};
 use coap::{Server, CoAPClient, CoAPRequest, IsMessage, MessageType, CoAPOption};
 use tokio::runtime::Runtime;
 
 #[bench]
 fn bench_server_with_request(b: &mut test::Bencher) {
-    thread::spawn(move || {
-		Runtime::new().unwrap().block_on(async move {
-            let mut server = Server::new("127.0.0.1:5683").unwrap();
+    let (tx, rx) = mpsc::channel();
 
-			server.run(move |request| {
+    thread::spawn(move || {
+        Runtime::new().unwrap().block_on(async move {
+            let mut server = Server::new("127.0.0.1:0").unwrap();
+
+            tx.send(server.socket_addr().unwrap().port()).unwrap();
+
+            server.run(move |request| {
                 let uri_path = request.get_path().to_string();
 
                 return match request.response {
@@ -23,22 +30,23 @@ fn bench_server_with_request(b: &mut test::Bencher) {
                     _ => None,
                 };
             }).await.unwrap();
-		});
-	});
-
-    let client = CoAPClient::new("127.0.0.1:5683").unwrap();
+        });
+    });
+    
+    let server_port = rx.recv().unwrap();
+    let client = CoAPClient::new(format!("127.0.0.1:{}", server_port)).unwrap();
 
     let mut request = CoAPRequest::new();
-	request.set_version(1);
-	request.set_type(MessageType::Confirmable);
-	request.set_code("0.01");
-	request.set_message_id(1);
-	request.set_token(vec!(0x51, 0x55, 0x77, 0xE8));
-	request.add_option(CoAPOption::UriPath, "test".to_string().into_bytes());
+    request.set_version(1);
+    request.set_type(MessageType::Confirmable);
+    request.set_code("0.01");
+    request.set_message_id(1);
+    request.set_token(vec!(0x51, 0x55, 0x77, 0xE8));
+    request.add_option(CoAPOption::UriPath, "test".to_string().into_bytes());
 
-	b.iter(|| {
-		client.send(&request).unwrap();
-		let recv_packet = client.receive().unwrap();
+    b.iter(|| {
+        client.send(&request).unwrap();
+        let recv_packet = client.receive().unwrap();
         assert_eq!(recv_packet.message.payload, b"test".to_vec());
-	});
+    });
 }
