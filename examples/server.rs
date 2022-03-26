0386b6414d77f79f592bdaa624336481e200a1f0
Aaron Tsui <aaron.tsui@outlook.com>
2022-03-26 16:34:17 +0800

    use #[tokio::main] macro

--- examples/server.rs
+++ examples/server.rs
@@ -2,39 +2,37 @@ extern crate coap;
 
 use coap::Server;
 use coap_lite::RequestType as Method;
-use tokio::runtime::Runtime;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let addr = "127.0.0.1:5683";
 
-    Runtime::new().unwrap().block_on(async move {
-        let mut server = Server::new(addr).unwrap();
-        println!("Server up on {}", addr);
+    let mut server = Server::new(addr).unwrap();
+    println!("Server up on {}", addr);
 
-        server
-            .run(|request| async {
-                match request.get_method() {
-                    &Method::Get => println!("request by get {}", request.get_path()),
-                    &Method::Post => println!(
-                        "request by post {}",
-                        String::from_utf8(request.message.payload).unwrap()
-                    ),
-                    &Method::Put => println!(
-                        "request by put {}",
-                        String::from_utf8(request.message.payload).unwrap()
-                    ),
-                    _ => println!("request by other method"),
-                };
+    server
+        .run(|request| async {
+            match request.get_method() {
+                &Method::Get => println!("request by get {}", request.get_path()),
+                &Method::Post => println!(
+                    "request by post {}",
+                    String::from_utf8(request.message.payload).unwrap()
+                ),
+                &Method::Put => println!(
+                    "request by put {}",
+                    String::from_utf8(request.message.payload).unwrap()
+                ),
+                _ => println!("request by other method"),
+            };
 
-                return match request.response {
-                    Some(mut message) => {
-                        message.message.payload = b"OK".to_vec();
-                        Some(message)
-                    }
-                    _ => None,
-                };
-            })
-            .await
-            .unwrap();
-    });
+            return match request.response {
+                Some(mut message) => {
+                    message.message.payload = b"OK".to_vec();
+                    Some(message)
+                }
+                _ => None,
+            };
+        })
+        .await
+        .unwrap();
 }
